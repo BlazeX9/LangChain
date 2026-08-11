a202c48bf33b6401a5bb2fc059a2bfd1b897660b
Abhik <chatterjeeabhik94@gmail.com>
2026-08-11 21:03:13 +0530

    11-08-2026

--- Agents/openai_multi_agent_basic.py
+++ Agents/openai_multi_agent_basic.py
@@ -1,13 +1,23 @@
+import streamlit
 from dotenv import load_dotenv
 import os
 from langchain_openai import ChatOpenAI
 from agent_tools import *
-import streamlit
 load_dotenv()
 
 llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"),model="gpt-4.1-mini",temperature=0)
-
-llm_tools = llm.bind_tools([AddTwoNumbers,MultiplyTwoNumbers,SaveToFile,ReadFromFile,DeleteFile,UpdateToFile,add_student,show_all_students,update_student,delete_student])
+llm_tools = llm.bind_tools([
+    AddTwoNumbers,
+    MultiplyTwoNumbers,
+    SaveToFile,
+    ReadFromFile,
+    DeleteFile,
+    UpdateToFile,
+    add_student,
+    show_all_students,
+    update_student,
+    delete_student
+])
 
 tool_map = {
     "AddTwoNumbers": AddTwoNumbers,
@@ -36,7 +46,7 @@ if user_submit and user_input:
 
             if tool_name in tool_map:
                 result = tool_map[tool_name].invoke(tool_args)
-                streamlit.write("**Tool used**:", tool_name)
+                #streamlit.write("**Tool used**:", tool_name)
                 streamlit.write("**Agent:**", result)
             else:
                 streamlit.write("Unknown tool:", tool_name)
